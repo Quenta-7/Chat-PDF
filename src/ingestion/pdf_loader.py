6a9b0959fb608dfb824c242d053c98556e07a9c4
Quenta-7 <126695829+Quenta-7@users.noreply.github.com>
2025-12-15 19:25:10 -0500

    cambio de sistema

--- src/ingestion/pdf_loader.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-def load_pdf(pdf_path: str):
-    """
-    Carga un PDF y devuelve una lista de Documentos LangChain
-    """
-    loader = PyPDFLoader(pdf_path)
-    documents = loader.load()
-    return documents
